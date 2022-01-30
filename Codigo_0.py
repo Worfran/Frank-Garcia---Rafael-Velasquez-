734d0be9aff8201079bc5027e1bc2166ddbdec8d
worfran <fraworman@hotmail.com>
2022-01-30 14:47:14 -0500

    Tarea 1 version 1

--- Codigo_0.py
+++ Codigo_0.py
@@ -5,22 +5,32 @@ import matplotlib as npl
 
 
 class particle():
-    def __init__(self,x_0,v_0, a_0,t, m, radius, Id):
+
+    def __init__(self, r,v, a,t, m,radius, Id, a=9.8):
         self.dt = t[1] - t[0] # creacion de atributo
         
-        self.r=x_0
-        self.v =v_0
+        self.g=a
+        self.x=r[0]
+        self.y=r[1]
+        self.vx =v[0]
+        self.vy =v[1]
         self.a=a_0
+        self.E=m*a*r[1]
 
-        self.rVector= np.zeros((len(t), len(x_0)))
+        #self.rVector= np.zeros((len(t), len(x)))
         self.vVector= np.zeros((len(t), len(v_0)))
         self.aVector= np.zeros((len(t), len(a_0)))
+        self.Evector=np.zeros((len(t), len()))
 
         self.m=m
         self.radius=radius
         self.Id=Id
+    
+    def colition(self):
+        None
 
-
+    def evolution(self):
+        None
 dt=0.01
 tmax=10
 t=np.arange(0,tmax+dt,dt)
