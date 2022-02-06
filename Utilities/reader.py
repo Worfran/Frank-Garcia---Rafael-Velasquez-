cd7c55b5fa5c6ce87afca23f23012f7e1cc2f4bd
worfran <fraworman@hotmail.com>
2022-02-06 17:24:48 -0500

    Primera version

---
+++ Utilities/reader.py
@@ -0,0 +1,12 @@
+import os.path as path
+import wget
+
+
+def reader(file, url):
+    if not path.exists(file):
+        Path_ = wget.download(url,file)
+    else:
+        Path_ = file
+    
+    df=np.loadtxt(Path_)
+    return df
