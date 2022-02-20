88e9263bb7718a0038b6f6433cedb1afaa00a583
worfran <fraworman@hotmail.com>
2022-02-19 22:11:28 -0500

    Tarea semana 4 inicio

---
+++ Puebas.py
@@ -0,0 +1,30 @@
+import numpy as np 
+
+Fo1=1
+Fo2=2
+Fo3=3
+Fo4=4
+
+it=range(0,4)
+
+K=[]
+
+for i in it:
+    k1=[]
+    k2=[]
+    k3=[]
+    k4=[]
+    Fi=[k1,k2,k3,k4]
+    K.append(Fi)
+
+
+t=np.arange(0,1,0.25)
+M=np.array([["Fo1","Fo2","Fo3","Fo4"],[Fo1,Fo2,Fo3,Fo4]])
+
+
+
+M=np.array([["Fo1","Fo2","Fo3","Fo4"],[Fo1,Fo2,Fo3,Fo4]])
+nd=[5,6,7,8]
+print(M[1,:])
+
+print(np.sqrt(9))
